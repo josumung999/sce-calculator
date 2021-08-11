5147874f485cbfb919dfbdb8158c55ce1cb878ee
Josué Munganga <jmunganga039@gmail.com>
2021-08-11 13:52:55 +0200

    Made major changes for new owner Easy Shopping, increased exchange rate to 1.49

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 var btnConvert = document.getElementById('btn-convert');
 
-var memberPriceDisplay = document.getElementById('member-price');
-var nonMemberPriceDisplay = document.getElementById('non-member-price');
-var memberPrice, nonMemberPrice, tshPrice;
+var priceDisplay = document.getElementById('price');
+var tshPrice, actualPrice;
 
 btnConvert.addEventListener('click', function (e) {
 	tshPrice = parseFloat(document.getElementById('tsh-price').value);
@@ -10,24 +9,20 @@ btnConvert.addEventListener('click', function (e) {
 	if (isNaN(tshPrice)) {
 		alert('Saisissez un nombre !')
 	} else {
-		var tshRate = 1.46;
+		var tshRate = 1.49;
 		var bifPrice = tshPrice * tshRate;
 		var margin1 = 0.3;
 		var margin2 = 0.2;
-		var memberDiscount = bifPrice * 0.03;
 
 		if (bifPrice <= 100000 ) {
-		 	nonMemberPrice = numeral(Math.ceil(bifPrice + bifPrice*margin1)).format('0,0.0');
-			memberPrice = numeral(Math.ceil((bifPrice + bifPrice*margin1) - memberDiscount)).format('0,0.0'); 
+		 	actualPrice = numeral(Math.ceil(bifPrice + bifPrice*margin1)).format('0,0.0'); 
 		} else if (bifPrice > 100000) {
-			nonMemberPrice = numeral(Math.ceil(bifPrice + bifPrice*margin2)).format('0,0.0');
-			memberPrice = numeral(Math.ceil((bifPrice + bifPrice*margin2) - memberDiscount)).format('0,0.0');
+			actualPrice = numeral(Math.ceil(bifPrice + bifPrice*margin2)).format('0,0.0');
 		} else {
 			alert("Ce nombre est négatif");
 		}
 
-		memberPriceDisplay.textContent = memberPrice + " BIF";
-		nonMemberPriceDisplay.textContent = nonMemberPrice + " BIF";
+		priceDisplay.textContent = actualPrice + " BIF";
 	}
 
 	e.preventDefault();
